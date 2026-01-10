1553452ba66edb73a318c9819c7d8813bb6ed116
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-01-09 19:32:13 -0800

    feat(home): back-to-top button added

--- madness/2026/main.js
+++ madness/2026/main.js
@@ -4,6 +4,7 @@ const content = document.getElementById('info-container');
 
 const menuNav = document.getElementById('menu');
 const overlay = document.getElementById('overlay');
+const home = document.getElementById('home');
 
 const scheduleTbl1 = document.getElementById('schedule-1');
 const scheduleTbl2 = document.getElementById('schedule-2');
@@ -107,6 +108,7 @@ function main() {
   for (const link of links) {
     link.addEventListener('click', closeMenu);
   }
+  home.addEventListener('click', closeMenu);
 }
 
 main();
