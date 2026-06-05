c0bce817a57527d8a97fccd8f7442d779ee4c51b
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-06-05 01:39:49 -0700

    feat: add Section components

--- packages/ui/src/types.ts
+++ packages/ui/src/types.ts
@@ -17,7 +17,7 @@ export const socialLinks = {
   instagram: 'https://www.instagram.com/sfu_csss/',
   linkedin: 'https://ca.linkedin.com/company/sfucsss',
   facebook: 'https://www.facebook.com/sfucsss'
-}
+};
 
 /**
  * The configuration for a website.
@@ -25,9 +25,11 @@ export const socialLinks = {
  * `name`: full name of the website, used for display purposes
  * `title`: title to be used in the <title> tag of the website
  * `description`: description of the website, used for SEO purposes
+ * `eventName`: full name of the event
  */
 export interface SiteConfig {
   name: string;
   title: string;
   description: string;
+  eventName: string;
 }
